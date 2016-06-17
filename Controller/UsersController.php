12ca0f5edd2e2b72257c7c4166929912e98ae3f8
Everton <evertonvieira1994@gmail.com>
2016-06-16 22:19:41 -0300

    Ajustando método para editar perfil

--- Controller/UsersController.php
+++ Controller/UsersController.php
@@ -255,25 +255,26 @@ class UsersController extends AmanagerAppController {
     }
   }
 
-    public function mycount(){
+    public function admin_mycont(){
 
-        $this->theme = 'Admin';
+			$this->theme = 'Admin';
 
-        $this->User->id = $this->Amanager->get_user_logged('id');
-        if (!$this->User->exists()) {
-            throw new NotFoundException(__('Invalid user'));
-        }
-        if ($this->request->is('post') || $this->request->is('put')) {
+			$this->User->id = $this->Amanager->get_user_logged('id');
+			if (!$this->User->exists()) {
+					throw new NotFoundException(__('Invalid user'));
+			}
+			if ($this->request->is('post') || $this->request->is('put')) {
 
-            if ($this->User->save($this->request->data)) {
-                $this->Session->setFlash(__d('amanager', 'Dados alterados com sucesso'), 'flash/success');
-                //$this->redirect(array('action' => 'index'));
-            } else {
-                $this->Session->setFlash(__d('amanager', 'The user could not be saved. Please, try again.'), 'flash/error');
-            }
-        } else {
-            $this->request->data = $this->User->read(null, $this->Amanager->get_user_logged('id'));
-        }
+					if ($this->User->save($this->request->data)) {
+							$this->Session->setFlash(__d('amanager', 'Dados alterados com sucesso'), 'flash/success');
+							//$this->redirect(array('action' => 'index'));
+					} else {
+							$this->Session->setFlash(__d('amanager', 'The user could not be saved. Please, try again.'), 'flash/error');
+					}
+			} else {
+					$this->request->data = $this->User->read(null, $this->Amanager->get_user_logged('id'));
+			}
+			$this->render("Settings/admin_index");
     }
 
   public function logout() {
